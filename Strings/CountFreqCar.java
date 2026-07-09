a0df9ae6eee7d6f4352475e2f0b07fd5e471d3bc
rishyandhark-sudo <rishyandhark@gmail.com>
2026-07-09 19:16:34 +0530

    Add frequency count for character 'a' in string
    Implement a program to count occurrences of 'a' in a string.

---
+++ Strings/CountFreqCar.java
@@ -0,0 +1,14 @@
+class Main{
+public static void main(String[] args){
+str="banana";
+//count a's in str
+int count = 0;
+
+for(int i = 0; i < str.length(); i++){
+
+    if(str.charAt(i) == 'a'){
+        count++;
+    }
+}
+}
+}
