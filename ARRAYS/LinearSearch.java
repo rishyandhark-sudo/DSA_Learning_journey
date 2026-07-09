bc8f9dbf8d77e94c6f5a898f596273fc77dcdb69
rishyandhark-sudo <rishyandhark@gmail.com>
2026-07-09 18:37:10 +0530

    Implement linear search in LinearSearch.java

---
+++ ARRAYS/LinearSearch.java
@@ -0,0 +1,22 @@
+class Search {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int k = 4;
+        boolean found = false;
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(arr[i] == k) {
+                found = true;
+                break;
+            }
+        }
+
+        if(found)
+            System.out.println("Found");
+        else
+            System.out.println("Not Found");
+    }
+}
