b131bb59a297ff073371431b126be6a8b1f662b2
rishyandhark-sudo <rishyandhark@gmail.com>
2026-07-09 19:08:22 +0530

    Remove unnecessary whitespace in ReverseUsingStringBuilder

---
+++ 01_Basics/ReverseUsingStringBuilder.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+      StringBuilder sb = new StringBuilder("hello");
+        sb.reverse();
+        System.out.print(sb);
+    }
+}
+
+        
+       
