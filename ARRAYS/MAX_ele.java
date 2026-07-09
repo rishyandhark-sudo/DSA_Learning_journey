a5cae928eb82798758205dd92267676d39973ea5
rishyandhark-sudo <rishyandhark@gmail.com>
2026-07-09 18:34:51 +0530

    Add Max class to find largest element in array

---
+++ ARRAYS/MAX_ele.java
@@ -0,0 +1,17 @@
+class Max {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int l = arr[0];
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(arr[i] > l) {
+                l = arr[i];
+            }
+        }
+
+        System.out.println("Largest one is " + l);
+    }
+}
