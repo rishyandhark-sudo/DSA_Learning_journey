93319022b77efe5319870a4640a1d26a7d0603ee
rishyandhark-sudo <rishyandhark@gmail.com>
2026-08-06 21:17:16 +0530

    Add subarraySum method to find subarray indices

---
+++ ARRAYS/IndexesofSubarraySum.java
@@ -0,0 +1,27 @@
+   class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int left = 0;
+        int sum = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            sum += arr[right];
+
+            while (sum > target && left <= right) {
+                sum -= arr[left];
+                left++;
+            }
+
+            if (sum == target) {
+                ans.add(left + 1); 
+                ans.add(right + 1);
+                return ans;
+            }
+        }
+
+        ans.add(-1);
+        return ans;
+    }
+}
+ 
