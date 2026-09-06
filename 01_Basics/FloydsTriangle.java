011bf35ecb4e63a033970514b7b68fdb4cb8a01d
rishyandhark-sudo <rishyandhark@gmail.com>
2026-09-06 20:26:14 +0530

    Add Floyd's Triangle implementation in Java

---
+++ 01_Basics/FloydsTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+int c=1;
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(c+" ");
+                c++;
+            }
+            System.out.println("");
+        }
+    }
+}
